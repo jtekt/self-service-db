bed2f89401ac69b4fc0aa3a2b2ef2bc3baf22f00
Maxime Moreillon <m.moreillon@gmail.com>
2024-08-24 12:08:19 +0900

    removed api based logic

--- middleware.ts
+++ middleware.ts
@@ -8,33 +8,6 @@ export async function middleware(request: NextRequest) {
   if (anonymousRoutes.includes(request.nextUrl.pathname)) return
 
   const userId = await getUserIdFromSession()
-
-  // TODO: also allow auth header
-
-  // const authHeader = request.headers.get("Authorization")
-
-  // if (!authHeader)
-  //   return NextResponse.json(
-  //     { message: `Authorization header not set` },
-  //     { status: 401 }
-  //   )
-
-  // const [_, token] = authHeader?.split(" ")
-
-  // if (!token)
-  //   return NextResponse.json({ message: `Token not found` }, { status: 401 })
-
-  // let verifiedJwt: any
-  // try {
-  //   verifiedJwt = await jose.jwtVerify(token, encodedJwtSecret)
-  // } catch (error) {
-  //   return NextResponse.json(
-  //     { message: `Token verification failed` },
-  //     { status: 401 }
-  //   )
-  // }
-  // const currentUser = verifiedJwt.payload
-
   if (!userId) return NextResponse.redirect(new URL("/login", request.nextUrl))
 
   return NextResponse.next()
