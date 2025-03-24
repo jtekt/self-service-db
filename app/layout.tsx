e0ca4dae0d4bec3ada76a7ffd24f85e5d9fb1094
Maxime Moreillon <m.moreillon@gmail.com>
2025-03-25 08:02:56 +0900

    Next.js vulnerability patch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
-import type { Metadata } from "next"
-import { Inter as FontSans } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils"
-import { PublicEnvScript } from "next-runtime-env"
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
+import { PublicEnvScript } from "next-runtime-env";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
   title: "Self DB",
   description: "Self service databases",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -37,10 +37,17 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <header className="p-4 text-2xl border-b">Self service DB</header>
-          <main className="max-w-3xl mx-auto p-4">{children}</main>
+          <div className="flex flex-col h-screen ">
+            <header className="p-4 text-2xl border-b">Self service DB</header>
+            <main className="w-full max-w-3xl mx-auto p-4 flex-grow">
+              {children}
+            </main>
+            <footer className="p-4 text-sm text-center border-t">
+              Self service DB | Maxime Moreillon | JTEKT Corporation
+            </footer>
+          </div>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
